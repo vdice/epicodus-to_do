93264ec6bb519fd3ae8d00ef0dd486d039a03ed4
Jing Wait & Vaughn Dice <student@epicodus.com>
2015-08-11 10:25:25 -0700

    test return path

--- spec/task_integration_spec.rb
+++ spec/task_integration_spec.rb
@@ -9,6 +9,16 @@ describe("the task path", {:type => :feature}) do
     fill_in("description", :with => "do dishes")
     click_button("Add task")
     expect(page).to have_content("The task has been successfully submitted!")
+  end
+end
 
+describe("the return path", {:type => :feature}) do
+  it("can return to the index page") do
+    visit("/")
+    fill_in("description", :with => "do dishes")
+    click_button("Add task")
+    click_link('Back')
+    expect(page).to have_content("Here are all your tasks")
+    expect(page).to have_content("do dishes")
   end
 end
